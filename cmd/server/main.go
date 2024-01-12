f06fa93b276e2a1c9eaf42f79e91e0dfed66cd7d
Pedro Monteiro <pedromont436@gmail.com>
2024-01-12 17:46:12 -0300

    criando handler para criar produto

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,88 @@
 package main
 
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/pedrohenmonteiro/golang-api/configs"
+	"github.com/pedrohenmonteiro/golang-api/internal/dto"
+	"github.com/pedrohenmonteiro/golang-api/internal/entity"
+	"github.com/pedrohenmonteiro/golang-api/internal/infra/database"
+)
+
 func main() {
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	autoMigrate(db)
+	if err != nil {
+		panic(err)
+	}
+
+	productDB := database.NewProduct(db)
+	productHandler := NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8080", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(p)
+}
+
+func autoMigrate(db *sql.DB) error {
+	_, err := db.Exec(`
+		CREATE TABLE IF NOT EXISTS products (
+			id TEXT PRIMARY KEY,
+			name TEXT,
+			price REAL,
+			created_at DATE
+		);
+		
+		CREATE TABLE IF NOT EXISTS users (
+			id TEXT PRIMARY KEY,
+			name TEXT,
+			email TEXT,
+			password TEXT
+		)
+	`)
+
+	return err
 }
