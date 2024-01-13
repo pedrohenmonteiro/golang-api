b0c690794375a419e2ba7df92b390ac141f92466
Pedro Monteiro <pedromont436@gmail.com>
2024-01-13 01:01:57 -0300

    corrigindo bug ao gerar token com email errado

--- internal/dto/dto.go
+++ internal/dto/dto.go
@@ -4,3 +4,14 @@ type CreateProductInput struct {
 	Name  string  `json:"name" binding:"required"`
 	Price float64 `json:"price" binding:"required"`
 }
+
+type CreateUserInput struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type GetJWTInput struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
