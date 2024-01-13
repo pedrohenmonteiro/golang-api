1b7830e1b0708a7e7723ae6d0bed87633ed494ea
Pedro Monteiro <pedromont436@gmail.com>
2024-01-12 21:54:27 -0300

    criando handler para listar e remover produto

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -35,8 +35,10 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products", productHandler.GetProducts)
 	r.Get("/products/{id}", productHandler.GetProduct)
 	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
 
 	// http.HandleFunc("/products", productHandler.CreateProduct)
 	http.ListenAndServe(":8080", r)
